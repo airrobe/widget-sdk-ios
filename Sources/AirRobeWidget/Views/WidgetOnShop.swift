ee96548c56b9c0bdcb0c62d91652c9bd1f1a6dcc
Eli Zhang <elizhang221@gmail.com>
2021-11-18 16:52:43 -0500

    Demo project scheme configured

---
+++ Sources/AirRobeWidget/Views/WidgetOnShop.swift
@@ -0,0 +1,14 @@
+//
+//  WidgetOnShop.swift
+//  
+//
+//  Created by King on 11/18/21.
+//
+
+import Foundation
+import UIKit
+
+// AirRobe widget which will be shown on Shopping page.
+class WidgetOnShop: UIView {
+    
+}
