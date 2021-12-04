fa1ab7ef5c4d5f6eebdfe99e34e37b82e229638f
Eli Zhang <elizhang221@gmail.com>
2021-12-03 19:54:33 -0500

    svg files added for logo, smile, check, plant

--- AirRobeWidget/Package.swift
+++ AirRobeWidget/Package.swift
@@ -7,17 +7,12 @@ let package = Package(
     name: "AirRobeWidget",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AirRobeWidget",
             targets: ["AirRobeWidget"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AirRobeWidget",
             dependencies: []),
